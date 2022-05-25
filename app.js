c62901e873ccec715bcbd315932a352498d25cf2
Tathagata <tathagatakar2015@gmail.com>
2022-05-25 19:07:37 +0530

    dynamic room fetch and search available room feature updated

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const session = require('express-session')
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static("public"));
 app.use(bodyParser.json())
 app.use(cookieParser('secret'))
-app.use(session({cookie: {maxAge: null}}))
+app.use(session({
+    cookie: {
+        maxAge: null
+    }
+}))
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.message = req.session.message
     delete req.session.message
     next()
-  })
+})
 
-mongoose.connect("mongodb://localhost:27017/hmsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/hmsDB", {
+    useNewUrlParser: true
+});
 
 const customerSchema = {
     name: String,
@@ -44,43 +52,41 @@ const Room = mongoose.model("room", roomSchema);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 })
 
-app.get('/signup', (req, res)=>{
+app.get('/signup', (req, res) => {
     res.render('signup');
 })
 
-app.post('/signup', (req, res)=>{
+app.post('/signup', (req, res) => {
 
     const cName = req.body.name;
     const cEmail = req.body.email;
     const cPassword = req.body.password;
     const cConfirm = req.body.confirm;
 
-    if(cName == '' || cEmail =='' || cPassword==''){
+    if (cName == '' || cEmail == '' || cPassword == '') {
         req.session.message = {
             type: 'danger',
             intro: 'Empty Credentials! ',
             message: 'Please insert the required details.'
-          }
-        res.redirect('/signup');
-    }
-    
-    else if(cPassword != cConfirm){
-        req.session.message = {
-          type: 'danger',
-          intro: 'Passwords do not match! ',
-          message: 'Please make sure to insert the same password.'
         }
         res.redirect('/signup');
-    }
-    
-    else {
-        Customer.findOne({email: cEmail}, function(err, found){
-            if (!err){
-                if (!found){
+    } else if (cPassword != cConfirm) {
+        req.session.message = {
+            type: 'danger',
+            intro: 'Passwords do not match! ',
+            message: 'Please make sure to insert the same password.'
+        }
+        res.redirect('/signup');
+    } else {
+        Customer.findOne({
+            email: cEmail
+        }, function (err, found) {
+            if (!err) {
+                if (!found) {
                     const customer = new Customer({
                         name: cName,
                         email: cEmail,
@@ -91,16 +97,14 @@ app.post('/signup', (req, res)=>{
                         type: 'success',
                         intro: 'Registration Successful! ',
                         message: 'Now you can Log in to book rooms.'
-                      }
+                    }
                     res.redirect('/signup');
-                } 
-                
-                else{
+                } else {
                     req.session.message = {
                         type: 'dark',
                         intro: 'Email already taken! ',
                         message: 'Please Log in.'
-                      }
+                    }
                     console.log("Email already taken, plz login")
                     res.redirect('/signup');
                 }
@@ -108,93 +112,101 @@ app.post('/signup', (req, res)=>{
         })
     }
 
-        
+
 
 });
 
-app.get('/login', (req, res)=>{
+app.get('/login', (req, res) => {
     res.render('login');
 })
 
-app.post('/login', (req, res)=>{
+app.post('/login', (req, res) => {
     const cEmail = req.body.email;
     const cPassword = req.body.password;
 
-    if(cEmail =='' || cPassword==''){
+    if (cEmail == '' || cPassword == '') {
         req.session.message = {
             type: 'danger',
             intro: 'Empty Credentials! ',
             message: 'Please insert the required details.'
-          }
+        }
         res.redirect('/login');
-    }
-    else{
-        Customer.findOne({email: cEmail}, function(err, found){
-            if (!err){
-                if (found){
-                    if (cPassword===found.password)
-                    {
+    } else {
+        Customer.findOne({
+            email: cEmail
+        }, function (err, found) {
+            if (!err) {
+                if (found) {
+                    if (cPassword === found.password) {
                         req.session.message = {
                             type: 'success',
                             intro: 'Succesfully logged in! ',
                             message: 'Enjoy.'
-                          }
+                        }
                         res.redirect('/login');
-                    }
-                    else{
+                    } else {
                         req.session.message = {
                             type: 'warning',
                             intro: 'You entered Wrong Password! ',
                             message: 'Enter again.'
-                          }
+                        }
                         res.redirect('/login');
                     }
-                    
-                } else{
+
+                } else {
                     req.session.message = {
                         type: 'info',
                         intro: 'You are new user! ',
                         message: 'Please register first.'
-                      }
+                    }
                     res.redirect('/login');
                 }
             }
         })
-    }  
+    }
 })
 
 
-app.get('/book', (req, res)=>{
-    
-    Room.find({}, function(err, roomDetails){
+app.get('/book', (req, res) => {
+
+    Room.find({}, function (err, roomDetails) {
+        if (err) {
+            return handleError(err);
+        } else {
+            res.render('book', {roomDetails:roomDetails});
+        }
+    });
+
+});
+
+app.post('/book', (req, res) => {
+    const person = req.body.quantity;
+    const date1 = req.body.checkInDate.toString();
+    const date2 = req.body.checkOutDate.toString();
+
+    const d1 = new Date(date1).toISOString();
+    const d2 = new Date(date2).toISOString();
+
+    Room.find({ 'capacity': 2, $or: [{$and: [{'checkIN': {$gt:d1 }}, {'checkIn':{$gt:d2}}]}, {$and: [{'checkOut': {$lt:d1 }}, {'checkOut':{$lt:d2}}]}]  }, 'roomNo', function(err, searchResult) {
         if (err){
             return handleError(err);
         } else{
-            res.render('book', {capa1:roomDetails[0].capacity, price1:roomDetails[0].price, type1:roomDetails[0].type, rating1:roomDetails[0].rating,
-                capa2:roomDetails[1].capacity, price2:roomDetails[1].price, type2:roomDetails[1].type, rating2:roomDetails[1].rating,
-                capa3:roomDetails[2].capacity, price3:roomDetails[2].price, type3:roomDetails[2].type, rating3:roomDetails[2].rating,
-                capa4:roomDetails[3].capacity, price4:roomDetails[3].price, type4:roomDetails[3].type, rating4:roomDetails[3].rating,
-                capa5:roomDetails[4].capacity, price5:roomDetails[4].price, type5:roomDetails[4].type, rating5:roomDetails[4].rating,
-                capa6:roomDetails[5].capacity, price6:roomDetails[5].price, type6:roomDetails[5].type, rating6:roomDetails[5].rating,
-                capa7:roomDetails[6].capacity, price7:roomDetails[6].price, type7:roomDetails[6].type, rating7:roomDetails[6].rating,
-                capa8:roomDetails[7].capacity, price8:roomDetails[7].price, type8:roomDetails[7].type, rating8:roomDetails[7].rating,
-                capa9:roomDetails[8].capacity, price9:roomDetails[8].price, type9:roomDetails[8].type, rating9:roomDetails[8].rating,
-            });
-        }});
-    
-});
-
-app.post('/book', (req, res)=>{
-    const person = req.body.quantity;
-    const type = req.body.roomType;
-    const date1 = req.body.checkInDate;
-    const date2 = req.body.checkOutDate;
-
+            if (searchResult){
+                console.log(searchResult);
+            } 
+            else {
+                console.log("No such room available right now");
+            }
+        }
+        
+    })
     res.redirect('/book');
 })
 
 
-
+app.get('/search', (req, res) => {
+    res.render('search');
+})
 
 
 app.listen(process.env.PORT || 3000);
